8d54f9ec09ea6fe8af1cfd53fc57e45e3adf9a40
praveen <saravananpraveen1157@gmail.com>
2025-09-02 18:45:05 +0530

    Practice: Py datetime, os, random, math, and itertools

--- Week-3/Day-21/Practice_itertools_module.py
+++ Week-3/Day-21/Practice_itertools_module.py
@@ -1,4 +1,5 @@
 import itertools
+from itertools import accumulate, chain, compress, pairwise, product, permutations, combinations
 
 r = range(1,10)
 i = iter(r)
@@ -36,3 +37,38 @@ repeat(object, times=None)
 Repeats an object multiple times.'''
 for i in itertools.repeat("hello",3):
     print(i)
+
+'''terminating iterator'''
+
+'''Running totals or custom accumulations.'''
+running_sum = accumulate(range(1,11))
+print(list(running_sum))
+
+'''Combines multiple iterables into one.'''
+chaining = chain("ABC","DEF")
+print(list(chaining))
+
+'''chain.from_iterable() is a variant of itertools.chain() that flattens a single iterable of iterables.'''
+iterchain = [[1,2,3],[4,5,6],[7,8,9]]
+print(list(chain.from_iterable(iterchain)))
+
+'''compress(data, selectors)
+Filters elements from data returning only those for which the corresponding element in selectors is True.'''
+data = [[1,2,3],['a','b','c'],[0,1,0]]
+print(list(compress(data,[True,False,True])))
+
+'''pairwise(iterable)
+Returns an iterator of pairs from the iterable.'''
+print(list(pairwise([1,2,3,4,5,5,6,7,8,9,10])))
+
+'''product(*iterable, repeat=1)
+Returns the Cartesian product of the input iterable.'''
+print(list(product([1,2,3],['a','b','c'])))
+
+'''permutations(iterable, r=None)
+Returns all possible permutations of the input iterable.'''
+print(list(permutations([1,2,3],2)))
+
+'''combinations(iterable, r)
+Returns all possible combinations of length r from the iterable.'''
+print(list(combinations([1,2,3],2)))
